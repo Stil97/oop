f6bb38d38cdc0d42e19130be072ffb8e530e7407
Stil97 <vanoxxxx@bk.ru>
2016-12-09 14:01:18 +0300

    Add files via upload

---
+++ Figure.h
@@ -0,0 +1,26 @@
+#ifndef FIGURE_H
+#define FIGURE_H
+
+#include <iostream>
+
+class Figure {
+public:
+	virtual double Square() = 0;
+	virtual void Print() = 0;
+	virtual ~Figure() {};
+
+	friend std::ostream& operator <<(std::ostream& os, Figure& fig) {
+		fig.Print();
+		return os;
+	}
+
+	friend bool operator <(Figure& fig1, Figure& fig2) {
+		return fig1.Square() < fig2.Square();
+	}
+
+	friend bool operator >(Figure& fig1, Figure& fig2) {
+		return fig1.Square() > fig2.Square();
+	}
+};
+
+#endif
